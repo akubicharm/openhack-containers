855aae2cf09a8cc18330eb6cd7e0cea2d5fcf77f
大溝桂 <kay@KeiOmizo-MacBook-Pro.local>
2021-01-09 21:08:51 +0900

    for postgresql

--- src/tripviewer/web/Utility/TripViewerConfiguration.cs
+++ src/tripviewer/web/Utility/TripViewerConfiguration.cs
@@ -5,6 +5,8 @@ namespace TripViewer.Utility
     {
         public string USERPROFILE_API_ENDPOINT { get; set; }
         public string TRIPS_API_ENDPOINT { get; set; }
+        // public string USERPROFILE_API_ENDPOINT =  "http://userprofile.api.svc.cluster.local";
+        // public string TRIPS_API_ENDPOINT = "http://trips.api.svc.cluster.local";
         public string BING_MAPS_KEY { get; set; }
     }
 }
